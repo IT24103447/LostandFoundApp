b72da4be7657d06924de3c59324b5e14d3874b70
IT24103447 <bimsaraanuda@gmail.com>
2026-09-09 04:25:08 +0530

    Add JMeter tests for found-item reporting

--- services/AuthService/Databases/Migrations/V007__add_deleted_at_to_users.sql
+++ services/AuthService/Databases/Migrations/V007__add_deleted_at_to_users.sql
@@ -1,14 +1,1 @@
-USE auth_service;
-
-SET @col_exists := (
-    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE TABLE_SCHEMA = 'auth_service'
-      AND TABLE_NAME = 'users'
-      AND COLUMN_NAME = 'deleted_at'
-);
-SET @sql := IF(@col_exists = 0,
-    'ALTER TABLE users ADD COLUMN deleted_at DATETIME(3) NULL DEFAULT NULL',
-    'SELECT 1');
-PREPARE stmt FROM @sql;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+ALTER TABLE users ADD COLUMN deleted_at DATETIME(3) NULL DEFAULT NULL;
